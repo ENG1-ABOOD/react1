85cc9a13f5dbe95160c7fa71ca71f65d11ccfce3
ENG1-ABOOD <abdalrhmanalsifi@gmail.com>
2024-12-27 19:55:44 +0300

    Add files via upload

---
+++ src/Features.jsx
@@ -0,0 +1,11 @@
+
+
+function Features() {
+  return (
+    <>    <h1>Features</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sunt eligendi, cumque neque quam at molestias odio aut maxime numquam doloribus facilis incidunt, amet repudiandae atque nemo. Quidem, voluptates laborum.</p>
+</>
+  )
+}
+
+export default Features
---
+++ src/Support.jsx
@@ -0,0 +1,10 @@
+
+function Support() {
+  return (
+    <>    <h1>Support</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sunt eligendi, cumque neque quam at molestias odio aut maxime numquam doloribus facilis incidunt, amet repudiandae atque nemo. Quidem, voluptates laborum.</p>
+</>
+  )
+}
+
+export default Support
